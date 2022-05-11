925f7f8cb93aa07161bf02dcbee9019c8fd14fea
Moise Tshiibwabwa <71385862+TshiMoiset@users.noreply.github.com>
2022-05-11 16:23:23 +0200

    Update navbar.js

--- JAVASCRIPT/navbar.js
+++ JAVASCRIPT/navbar.js
@@ -29,23 +29,4 @@ function closeNavbar() {
     }
 }
 
-/*------------------- Scroll -------------------*/
 
-let scrollUp = window.pageYOffset;
-
-window.onscroll = function () {
-    let scrollDown = window.pageYOffset;
-    if (scrollUp > scrollDown) {
-        document.getElementById("navBar").style.top = "0";
-
-        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-            document.getElementById("navBar").style.backgroundColor = "Black"
-            document.getElementById("screenLogo-NavButton").style.backgroundColor = "Black";
-        }
-
-    } else {
-        document.getElementById("navBar").style.top = "-100px"
-    }
-
-    scrollUp = scrollDown;
-}
